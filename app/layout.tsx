76ddd4d0fe0360de685457a7999b3cbcdae9f2f4
sami-bytequest <samiullah.bytequests@gmail.com>
2024-09-08 22:49:51 +0530

    toast setup & wrap app with auth provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
+import { Toaster } from "react-hot-toast";
+import AuthProvider from "@/components/auth-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} bg-black/95`}>
-        <Header />
-        {children}
+        <AuthProvider>
+          <Header />
+          {children}
+          <Toaster position="top-center" reverseOrder={false} />
+        </AuthProvider>
       </body>
     </html>
   );
