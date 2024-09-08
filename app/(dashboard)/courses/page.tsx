b2e656f15512cf797a12abae41b3bd6f2ae133cd
sami-bytequest <samiullah.bytequests@gmail.com>
2024-09-08 23:26:06 +0530

    courses page

--- app/(dashboard)/courses/page.tsx
+++ app/(dashboard)/courses/page.tsx
@@ -2,17 +2,25 @@
 import { endpoints } from "@/utils/endpoints";
 import http from "@/utils/http";
 import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+
+type CourseType = {
+  title: string;
+  duration: number;
+  _id: string;
+};
 
 export default function Courses() {
   const [isLoading, setIsLoading] = useState(true);
-  const [data, setData] = useState();
+  const [courses, setCourses] = useState<CourseType[]>([]);
 
   useEffect(() => {
     const getCourses = async () => {
       try {
         const res = await http().get(endpoints.dashbaord.courses);
-      } catch (error) {
-        console.log(error);
+        setCourses(res?.data);
+      } catch (error: any) {
+        toast.error(error?.message);
       } finally {
         setIsLoading(false);
       }
@@ -20,7 +28,26 @@ export default function Courses() {
     getCourses();
   }, []);
 
-  console.log(data);
+  if (isLoading) {
+    return (
+      <div className="w-full h-full flex items-center justify-center">
+        <p className="text-gray-300">Loading...</p>
+      </div>
+    );
+  }
 
-  return <div>Courses</div>;
+  return (
+    <div className="px-4 lg:px-0 max-w-2xl mx-auto space-y-3 mt-10">
+      {courses?.map((course, i) => (
+        <div
+          key={course._id}
+          className="px-4 py-3 bg-slate-800 flex items-center gap-5"
+        >
+          <p className="text-base md:text-lg text-white">
+            {course.title} <span>{`(${course.duration} hours)`}</span>
+          </p>
+        </div>
+      ))}
+    </div>
+  );
 }
